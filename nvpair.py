db2e21fdc34b499c285cba46158e8f58f6f2a3cf
Cyril Plisko <cyril.plisko@grigale.com>
2011-06-30 12:27:23 +0300

    Implement class nvpair

--- nvpair.py
+++ nvpair.py
@@ -90,8 +90,86 @@ def nvl2dict(nvl):
     return _data
 
 
-class NVPair():
-    pass
+class nvpair():
+    def __init__(self, nvp=None):
+        self._nvp = nvp
+
+    def name(self):
+        return libnvpair.nvpair_name(self._nvp) if self._nvp else None
+
+    def type(self):
+        return libnvpair.nvpair_type(nvp) if self._nvp else None
+
+    def value(self):
+        type = self.type()
+        value = C.c_void_p()
+        if type == libnvpair.DATA_TYPE_BOOLEAN:
+            pass
+        elif type == libnvpair.DATA_TYPE_BYTE:
+            value = C.c_byte()
+            libnvpair.nvpair_value_byte(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_INT16:
+            value = C.c_int16()
+            libnvpair.nvpair_value_int16(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_UINT16:
+            value = C.c_uint16()
+            libnvpair.nvpair_value_uint16(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_INT32:
+            value = C.c_int32()
+            libnvpair.nvpair_value_int32(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_UINT32:
+            value = C.c_uint32()
+            libnvpair.nvpair_value_uint32(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_INT64:
+            value = C.c_int64()
+            libnvpair.nvpair_value_int64(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_UINT64:
+            value = C.c_uint64()
+            libnvpair.nvpair_value_uint64(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_STRING:
+            value = C.c_char_p()
+            libnvpair.nvpair_value_string(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_BYTE_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_INT16_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_UINT16_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_INT32_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_UINT32_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_INT64_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_UINT64_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_STRING_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_HRTIME:
+            pass
+        elif type == libnvpair.DATA_TYPE_NVLIST:
+            pass
+        elif type == libnvpair.DATA_TYPE_NVLIST_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_BOOLEAN_VALUE:
+            pass
+        elif type == libnvpair.DATA_TYPE_INT8:
+            value = C.c_int8()
+            libnvpair.nvpair_value_int8(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_UINT8:
+            value = C.c_uint8()
+            libnvpair.nvpair_value_uint8(nvp, C.byref(value))
+        elif type == libnvpair.DATA_TYPE_BOOLEAN_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_INT8_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_UINT8_ARRAY:
+            pass
+        elif type == libnvpair.DATA_TYPE_DOUBLE:
+            pass
+
+        return value.value
+
 
 class NVList(UserDict.IterableUserDict):
     def __init__(self, nvl):
